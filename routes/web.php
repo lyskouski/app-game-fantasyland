19f6f151cfc13253edca1cbce406119e6810ffb7
Viachaslau Lyskouski <deus@creativity.by>
2026-02-25 09:00:34 +0100

    [#2] [NF] Authentication. Initial routing

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('/index.php', function () {
+    return view('login');
+});
+
+Route::get('/login.php', function () {
+    return view('main');
 });
