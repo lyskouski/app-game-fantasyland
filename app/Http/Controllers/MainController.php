a80c7772a8b083fba60f6b1d76d0fb2db2e60250
Viachaslau Lyskouski <deus@creativity.by>
2026-03-02 19:32:52 +0100

    [#1] [NF] Movements in location. Movements inside locations

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -20,8 +20,10 @@ class MainController extends Controller
         $html = $this->curl->boot('https://www.fantasyland.ru/cgi/no_combat.php', $post);
         if (strpos($html, 'id="LocTable"') !== false) {
             return view('main_location', $this->onLocation($html));
+        } elseif (strpos($html, 'cssLocImage') !== false) {
+            return view('main_place', $this->onPlace($html));
         }
-        return view('generic');
+        return view('generic', ['data' => $html]);
     }
 
     private function onLocation(string $html) {
@@ -38,6 +40,43 @@ class MainController extends Controller
             $image = $imgMatch[1];
         }
         return ['map' => $map, 'image' => $image];
+    }
+
+    private function onPlace(string $html) {
+        $title = '';
+        if (preg_match('/show_title\(["\']([^"\']+)["\']\)/', $html, $titleMatch)) {
+            $title = html_entity_decode($titleMatch[1]);
+        } elseif (preg_match('/<script>document.write\(show_title\(["\']([^"\']+)["\']\)\);<\/script>/i', $html, $titleMatch)) {
+            $title = html_entity_decode($titleMatch[1]);
+        }
+        $image = '';
+        if (preg_match('/<image[^>]*class=(["\'])?cssLocImage\1?[^>]*src=(["\'])([^"\']+)\2/i', $html, $imgMatch)) {
+            $image = str_replace('..', '', $imgMatch[3]);
+        }
+        $map = [];
+        if (preg_match_all("/<A[^>]*HREF=['\"]javascript:goTo\((\d+)\)['\"][^>]*>.*?<\/A>.*?<TD>([^<]+)<\/TD>/is", $html, $goToMatches, PREG_SET_ORDER)) {
+            foreach ($goToMatches as $m) {
+                $id = (int)$m[1];
+                $loc = trim($m[2]);
+                $map[] = ['loc' => $loc, 'id' => $id];
+            }
+        }
+        if (preg_match_all('/<button[^>]+onClick=["\']goTo\((\d+)\)["\'][^>]*>([^<]*)<\/button>/i', $html, $btnMatches, PREG_SET_ORDER)) {
+            foreach ($btnMatches as $m) {
+                $loc = trim($m[2]);
+                $id = (int)$m[1];
+                $map[] = ['loc' => $loc ?: 'Обновить', 'id' => $id];
+            }
+        }
+        $place = [];
+        if (preg_match_all("/<A[^>]*HREF=['\"]javascript:regimeTo\((\d+)\)['\"][^>]*>.*?<\/A>.*?<TD>([^<]+)<\/TD>/is", $html, $regimeToMatches, PREG_SET_ORDER)) {
+            foreach ($regimeToMatches as $m) {
+                $id = (int)$m[1];
+                $loc = trim($m[2]);
+                $place[] = ['loc' => $loc, 'id' => $id];
+            }
+        }
+        return ['image' => $image, 'title' => $title, 'map' => $map, 'place' => $place];
     }
 
 }
---
+++ resources/views/main_place.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Лига Героев</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        @vite(['resources/css/login.css'])
+    </head>
+    <body>
+        <br />
+        <br />
+        <div class="main">
+            <div class="main_top">
+                <table cellpadding="0" cellspacing="0" align="center">
+                    <tr>
+                        <td>
+                            <img src="https://www.fantasyland.ru/images/buttons/tab_l.gif" width="30" height="15" />
+                        </td>
+                        <td valign="top" class="cell_title">
+                            <small><b>&nbsp;{{ $title }}&nbsp;</b></small>
+                        </td>
+                        <td>
+                            <img src="https://www.fantasyland.ru/images/buttons/tab_r.gif" width="30" height="15" />
+                        </td>
+                    </tr>
+                </table>
+                <br />
+            </div>
+            <div class="main_middle">
+                <img src="https://www.fantasyland.ru/{{ $image }}" width="100%" />
+            </div>
+        </div>
+        <br />
+        <div class="main">
+            <div class="main_top">
+                <table cellpadding="0" cellspacing="0" align="center">
+                    <tr>
+                        <td>
+                            <img src="https://www.fantasyland.ru/images/buttons/tab_l.gif" width="30" height="15">
+                        </td>
+                        <td valign="top" class="cell_title">
+                            <small><b>&nbsp;Локации для перехода&nbsp;</b></small>
+                        </td>
+                        <td>
+                            <img src="https://www.fantasyland.ru/images/buttons/tab_r.gif" width="30" height="15" />
+                        </td>
+                    </tr>
+                </table>
+                <br />
+            </div>
+            <div class="main_middle">
+                @foreach ($place as $location)
+                <form method="POST" action="/cgi/no_combat.php" style="margin-bottom: 8px;">
+                    @csrf
+                    <input type="hidden" name="place_regime" value="{{ $location['id'] }}" />
+                    <input type="hidden" name="addval" value=0 />
+                    <input type="hidden" name="addval1" value=0 />
+                    <input type="submit" value="{{ $location['loc'] }}" style="width: 100%;" />
+                </form>
+                @endforeach
+                <br />
+                @foreach ($map as $location)
+                <form method="POST" action="/cgi/no_combat.php" style="margin-bottom: 8px;">
+                    @csrf
+                    <input type="hidden" name="locat" value="{{ $location['id'] }}" />
+                    <input type="hidden" name="additional" value="0" />
+                    <input type="submit" value="{{ $location['loc'] }}" style="width: 100%;" />
+                </form>
+                @endforeach
+            </div>
+        </div>
+        <br />
+    </body>
+</html>
