06d5d3f24b03148c6d430ec4f128ca25a9c3755d
Viachaslau Lyskouski <deus@creativity.by>
2026-04-04 19:03:31 +0200

    [#15] [NF] Labyrinth. Preserve connection

--- resources/views/labyrinth_quest.blade.php
+++ resources/views/labyrinth_quest.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-        @vite(['resources/css/index.css'])
+        @vite(['resources/css/index.css', 'resources/js/ping.js'])
     </head>
     <body>
         <br />
