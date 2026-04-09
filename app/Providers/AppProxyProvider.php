7aa4195648daa9f6ca9a88ec795240fba4ec0076
Viachaslau Lyskouski <deus@creativity.by>
2026-04-09 13:44:16 +0200

    [#40] [CI] Unable to login. Fix

--- app/Providers/AppProxyProvider.php
+++ app/Providers/AppProxyProvider.php
@@ -6,7 +6,7 @@ namespace App\Providers;
 
 class AppProxyProvider
 {
-    protected $browser = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3';
+    protected $browser = 'Mozilla/5.0 (X11; Linux x86_64; rv:149.0) Gecko/20100101 Firefox/149.0';
     protected $fcurl;
 
     public function __construct()
