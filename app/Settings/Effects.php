2d6b9c3e74abaa9cecd0d565a86ef59cd21de145
Viachaslau Lyskouski <deus@creativity.by>
2026-05-07 18:41:23 +0200

    [#59] [BP] Healthcheck automation. Improve effects

--- app/Settings/Effects.php
+++ app/Settings/Effects.php
@@ -8,70 +8,72 @@ class Effects
 {
     public static function getImage(string $str): string
     {
-        $sNew = '';
-        $sText = strtolower($str);
-        $sImgBegin = '&nbsp;<img align=absmiddle width=14 height=14 src="' .
-            Defines::URL . 'images/miscellaneous/';
-        $nNum = explode(' ', $str)[0];
-
-        if (strpos($sText, ',') !== false) {
-            $aNew = explode(',', $sText);
-            foreach ($aNew as $item) {
-                $cleaned = preg_replace('/(?:(?:^|\n)\s+|\s+(?:$|\n))/u', '', $item);
-                $cleaned = preg_replace('/\s+/u', ' ', $cleaned);
-                $sNew .= self::getImage($cleaned) . ' ';
+        if (str_contains($str, ',')) {
+            $items = explode(',', $str);
+            $results = [];
+            foreach ($items as $item) {
+                $item = preg_replace('/(?:(?:^|\n)\s+|\s+(?:$|\n))/u', '', $item);
+                $item = preg_replace('/\s+/u', ' ', $item);
+                $results[] = self::getImage($item);
             }
-            return $sNew;
-        } elseif (strpos($sText, 'ции жизни') !== false) {
-            $sNew = '../effects/circle_of_life';
-        } elseif (strpos($sText, 'жизни') !== false) {
-            $sNew = 'hp';
-        } elseif (strpos($sText, 'эффекто') !== false) {
-            $sNew = 'm_prot';
-        } elseif (strpos($sText, 'силы') !== false) {
-            $sNew = 'strength';
-        } elseif (strpos($sText, 'удаче') !== false) {
-            $sNew = 'luck';
-        } elseif (strpos($sText, 'ума') !== false) {
-            $sNew = 'intelligence';
-        } elseif (strpos($sText, 'скорости') !== false) {
-            $sNew = 'speed';
-        } elseif (strpos($sText, 'концентр') !== false) {
-            $sNew = 'conc';
-        } elseif (strpos($sText, 'от дам и колдовства') !== false) {
-            $sNew = 'defence_l.gif" title="' . $str . '" /><br />' . $nNum . $sImgBegin . 'defence_s';
-        } elseif (strpos($sText, 'от рыцарей и света') !== false) {
-            $sNew = 'defence_k.gif" title="' . $str . '" /><br />' . $nNum . $sImgBegin . 'defence_h';
-        } elseif (strpos($sText, 'от драконов и хаоса') !== false) {
-            $sNew = 'defence_d.gif" title="' . $str . '" /><br />' . $nNum . $sImgBegin . 'defence_c';
-        } elseif (strpos($sText, 'от дам') !== false) {
-            $sNew = 'defence_l';
-        } elseif (strpos($sText, 'от рыцарей') !== false) {
-            $sNew = 'defence_k';
-        } elseif (strpos($sText, 'от драконов') !== false) {
-            $sNew = 'defence_d';
-        } elseif (strpos($sText, 'от хаоса') !== false) {
-            $sNew = 'defence_c';
-        } elseif (strpos($sText, 'от света') !== false) {
-            $sNew = 'defence_h';
-        } elseif (strpos($sText, 'от яда') !== false) {
-            $sNew = 'pp';
-        } elseif (strpos($sText, 'от колдовства') !== false) {
-            $sNew = 'defence_s';
-        } elseif (strpos($sText, 'дам') !== false) {
-            $sNew = 'attack_l';
-        } elseif (strpos($sText, 'рыцар') !== false) {
-            $sNew = 'attack_k';
-        } elseif (strpos($sText, 'дракон') !== false) {
-            $sNew = 'attack_d';
-        } elseif (strpos($sText, 'хаос') !== false) {
-            $sNew = 'attack_c';
-        } elseif (strpos($sText, 'свет') !== false) {
-            $sNew = 'attack_h';
-        } elseif (strpos($sText, 'колдовств') !== false) {
-            $sNew = 'attack_s';
+            return implode(' ', $results);
         }
 
-        return $sNew ? $nNum . $sImgBegin . $sNew . '.gif" title="' . $str . '" />' : $str;
+        $text = strtolower($str);
+        $firstNumber = explode(' ', $str)[0];
+
+        foreach (self::getEffectPatterns() as $pattern => $effect) {
+            if (str_contains($text, $pattern)) {
+                return self::buildImageTag($firstNumber, $effect, $str);
+            }
+        }
+
+        return $str;
+    }
+
+    /**
+     * @return array<string, string|null>
+     */
+    private static function getEffectPatterns(): array
+    {
+        return [
+            'ции жизни' => '../effects/circle_of_life',
+            'жизни' => 'hp',
+            'эффекто' => 'm_prot',
+            'силы' => 'strength',
+            'удаче' => 'luck',
+            'ума' => 'intelligence',
+            'скорости' => 'speed',
+            'концентр' => 'conc',
+            'от дам и колдовства' => 'defence_l|defence_s',
+            'от рыцарей и света' => 'defence_k|defence_h',
+            'от драконов и хаоса' => 'defence_d|defence_c',
+            'от дам' => 'defence_l',
+            'от рыцарей' => 'defence_k',
+            'от драконов' => 'defence_d',
+            'от хаоса' => 'defence_c',
+            'от света' => 'defence_h',
+            'от яда' => 'pp',
+            'от колдовства' => 'defence_s',
+            'дам' => 'attack_l',
+            'рыцар' => 'attack_k',
+            'дракон' => 'attack_d',
+            'хаос' => 'attack_c',
+            'свет' => 'attack_h',
+            'колдовств' => 'attack_s',
+        ];
+    }
+
+    private static function buildImageTag(string $number, string $effect, string $str): string
+    {
+        $imgBegin = '&nbsp;<img align=absmiddle width=14 height=14 src="' . Defines::URL . 'images/miscellaneous/';
+
+        if (str_contains($effect, '|')) {
+            [$effect1, $effect2] = explode('|', $effect);
+            return $number . $imgBegin . $effect1 . '.gif" title="' . $str . '" /><br />' .
+                   $number . $imgBegin . $effect2 . '.gif" title="' . $str . '" />';
+        }
+
+        return $number . $imgBegin . $effect . '.gif" title="' . $str . '" />';
     }
 }
