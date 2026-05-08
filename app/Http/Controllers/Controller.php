9ff155e5ce884ad1f377f5b5081a5a3fdb161127
Viachaslau Lyskouski <deus@creativity.by>
2026-05-08 08:55:32 +0200

    [#59] [BP] Healthcheck automation. Improve generic

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -19,17 +19,33 @@ abstract class Controller
     public function generic(string $url, ?array $get = null, ?array $post = null) {
         $addr = Defines::URL;
         $data = $this->curl->boot($addr . $url, $get, $post);
-        $data = str_replace('src="../', 'src="' . $addr, $data);
-        $data = str_replace('SRC="../', 'src="' . $addr, $data);
-        $data = str_replace('src=../', 'src=' . $addr, $data);
-        $data = str_replace('src="/', 'src="' . $addr, $data);
-        $data = str_replace('src=/', 'src=' . $addr, $data);
-        $data = str_replace('src="images/', 'src="' . $addr . 'images/', $data);
-        $data = str_replace("BACKGROUND='../", "background='" . $addr, $data);
-        $data = str_replace('BACKGROUND="../', 'background="' . $addr, $data);
-        $data = str_replace('background="../', 'background="' . $addr, $data);
+
+        // Single regex pass to handle all src/background URL replacements
+        $data = preg_replace_callback(
+            '/(src|background)\s*=\s*(["\']?)(?:\.\.\/|\/)?(?!https?:\/\/)([^"\'\s>]*)/i',
+            function ($matches) use ($addr) {
+                $prefix = $matches[1];  // 'src' or 'background'
+                $quote = $matches[2];
+                $path = $matches[3];
+
+                // Add URL prefix if path is relative
+                if (!preg_match('~^(?:https?://|/)~', $path)) {
+                    $path = $addr . $path;
+                }
+
+                return "{$prefix}={$quote}{$path}";
+            },
+            $data
+        );
+
+        // Handle other replacements
         $data = str_replace('target="_BLANK"', '', $data);
-        $data = str_replace("window.open('help','_blank','scrollbars=yes,width=900,height=560,resizable=yes')", "document.location.href='/help/enc.php?type=menu'", $data);
+        $data = str_replace(
+            "window.open('help','_blank','scrollbars=yes,width=900,height=560,resizable=yes')",
+            "document.location.href='/help/enc.php?type=menu'",
+            $data
+        );
+
         return view('generic', ['data' => $data]);
     }
 
